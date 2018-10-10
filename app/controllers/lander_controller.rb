5b0c7ad183e8a7ee435a2e1f870e909400c4a56c
ErichSA <erich@gmail.com>
2018-10-10 20:41:11 +0300

    Signed-off-by: ErichSA <erich@gmail.com>

--- app/controllers/lander_controller.rb
+++ app/controllers/lander_controller.rb
@@ -1,6 +1,11 @@
 class LanderController < ApplicationController
 
   def index
+    @profile = Profile.new
+  end
+
+  def sign_up
+    Profile.create!(email: params['email']);
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :lander, only: [:index]
+
   root to: 'lander#index'
+  post 'lander/sign_up' => 'lander#sign_up', as: 'profile_sign_up'
 end
